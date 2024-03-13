907c2ad783ee49a2779e5777a0d6560a5d96135a
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-03-13 17:22:27 -0300

    Add files via upload

---
+++ 2-SEM-PROGRAMACAO-WEB/Matrizes.php
@@ -0,0 +1,30 @@
+<?php
+	// Matrizes.php
+	$nome="Carlos";
+	$nome2="Ana";
+	$nome3="Roberta";
+	//-----------------------
+	$nomes[1]="Carlos";
+	$nomes[2]="Ana";
+	$nomes[3]="Roberta";
+	$nomes[4]="Marcos";
+
+	echo $nomes[2] . " é esposa de ". $nomes[4];
+	
+	//------------------------
+	$funcionarios[1][1]="Carlos";
+	$funcionarios[1][2]=8750.20;
+	$funcionarios[1][3]="Diretor";
+	
+	$funcionarios[2][1]="Ana";
+	$funcionarios[2][2]=5700.95;
+	$funcionarios[1][3]="Gerente";
+	
+	$funcionarios[3][1]="Roberta";
+	$funcionarios[3][2]=1497.60;
+	$funcionarios[1][3]="Office-Girl";
+	
+	$funcionarios[4][1]="Marcos";
+	$funcionarios[4][2]=2920.12;
+	$funcionarios[1][3]="Assistente";
+?>
