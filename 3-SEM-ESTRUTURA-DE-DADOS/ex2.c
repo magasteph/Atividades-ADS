5d95644e2484aeee77bcd37b40b83099ad77b2c1
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-03-14 11:53:40 -0300

    Create ex2.c

---
+++ 3-SEM-ESTRUTURA-DE-DADOS/ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int mat[3][3]={{1,2,3},
+                {4,5,6},
+                {7,8,9}};
+  int i, j;
+  for (i=0; i<3; i++){
+    for (j=0; j<3; j++){
+      printf("%d ", mat[i][j]);
+    }
+    printf("\n");
+  }
+}
