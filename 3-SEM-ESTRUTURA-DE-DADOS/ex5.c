0268bb7b426ad5d56b51cf93d916d69d7572a7d5
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-03-15 12:18:56 -0300

    Update ex5.c

--- 3-SEM-ESTRUTURA-DE-DADOS/ex5.c
+++ 3-SEM-ESTRUTURA-DE-DADOS/ex5.c
@@ -1,14 +1,16 @@
 #include <stdio.h> 
 int main(){
-  int MatrizA = {{1,2,5,6},
-                {5,8,4,9},
-                {5,2,8,4},
-                {8,4,7,1}}
+  int MatrizA[4][4] = {{1,2,5,6},
+                      {5,8,4,9},
+                     {5,2,8,4},
+                      {8,4,7,1}};
+
 int i;
 int soma = 0;
-for (i=0;i<4;i++){
-  soma += MatrizA[2][i];
-}
-printf("A soma das matrizes da linha 3 é de: ", soma);
-return 0        
+
+  for(i=0;i<4;i++){
+    soma += MatrizA[2][i];
+  }
+printf("A soma das matrizes da linha 3 é de: %d \n", soma);
+return 0 ;      
 };
