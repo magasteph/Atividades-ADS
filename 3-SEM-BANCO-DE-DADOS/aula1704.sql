c018ab4ba8c106ec94d3d54f2a66250ff316dcea
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-04-18 17:00:36 -0300

    Update aula1704.sql

--- 3-SEM-BANCO-DE-DADOS/aula1704.sql
+++ 3-SEM-BANCO-DE-DADOS/aula1704.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE revisão;
+CREATE DATABASE if not exists revisão;
 USE revisão;
 
 CREATE TABLE if not exists fabricante(
@@ -33,11 +33,33 @@ insert into produto(nome_prod) values ('Galaxy S24'),('Redmi Note 9'), ('A50'), 
 
 select * from produto;
 
-insert into produto(nome_prod, fk_COD, situacao) values ('Galaxy S23',1,'INA'), ('Galaxy S22',1,'INA'),('POCO FONE X5',2,'ATI'),('Redmi Note 10',2,'ATI');
+insert into produto(nome_prod, fk_COD, situacao) values ('Galaxy S23',1,'INA'),
+	('Galaxy S22',1,'INA'),('POCO FONE X5',2,'ATI'),('Redmi Note 10',2,'ATI');
 
 use faculdade;                                                       
 select * from revisão.produto as prod;
 use revisão;                               
 
 select substring('botafogo campeão', 10, 7);                     
-select nome_prod, substring(nome_prod,2,3) from produto;
+select lower(nome_prod) as Produtão, 
+    substring(upper(nome_prod),2) as 'Resumo do Produto',
+    repeat(nome_prod,4) as 'Inútil'
+from produto;
+insert into produto (nome_prod) values 
+       ('Galaxy Poco Note 22'),('Redmi Note 17'),('A50'),('G20');
+
+
+select nome_prod, 
+substring(nome_prod,instr(nome_prod,'note'),50)
+from produto;
+
+alter table produto add column dt_lanc date;
+alter table produto drop column dt_lanc;
+alter table produto add column dt_lanc date;
+
+describe produto;
+insert into produto (nome_prod, dt_lanc) values 
+       ('Galaxy Poco Note 22', curdate()),
+       ('Redmi Note 17',curdate()),
+       ('A50','2023-05-01'),('G20','20231118');
+select * from produto;
