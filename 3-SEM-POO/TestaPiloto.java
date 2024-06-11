9e3c0c80400947284550828ac9776990864c79a3
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-06-11 10:35:44 -0300

    Create TestaPiloto.java

---
+++ 3-SEM-POO/TestaPiloto.java
@@ -0,0 +1,10 @@
+public class TestaPiloto {
+    public static void main(String[] args) {
+        Piloto pt1 = new Piloto();
+
+        pt1.nomePiloto = "Stephanne";
+    
+        pt1.adicionarHoras(300);
+        pt1.imprimirDados();
+    }
+}
