254f6a0155cebb5f8972e8277aaae3c4466b599c
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-06-11 10:36:45 -0300

    Create TestaArma.java

---
+++ 3-SEM-POO/TestaArma.java
@@ -0,0 +1,11 @@
+public class TestaArma {
+    public static void main(String[] args) {
+        Arma arma1 = new Arma('A');
+        Arma arma2 = new Arma('D');
+        Arma arma3 = new Arma('L');
+
+        arma1.imprimeDados();
+        arma2.imprimeDados();
+        arma3.imprimeDados();
+    }
+}
