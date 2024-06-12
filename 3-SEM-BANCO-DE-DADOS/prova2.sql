8fe6b49fe3bf090ddcd59b89095b94e7d04afcae
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-06-12 16:37:18 -0300

    Update prova2.sql

--- 3-SEM-BANCO-DE-DADOS/prova2.sql
+++ 3-SEM-BANCO-DE-DADOS/prova2.sql
@@ -12,4 +12,23 @@ alter table cursos add column idcurso int first;
 
 alter table cursos add primary key (idcurso);
 
+insert into cursos values ('1', 'HTML4', 'Curso de Html5', '40', '37','2014'), 
+('2', 'Algoritmos', 'Logica de Programacao','20', '15','2014'),
+('3','Photoshop', 'Dicas de Photo', '10','8','2014'),
+('4','pgp','curso de php','40','20','2010'),
+  ('5', 'Jarva', 'Introbucao', '50','15','2014');
 
+select * from cursos;
+
+update cursos set nome = 'HTML5' where idcurso = '1';
+
+update cursos set nome = 'PHP', ano = '2015' where idcurso = '4';
+
+update cursos set nome = 'Java', descricao = 'Introdução', 
+  carga = '40', ano = '2015' where idcurso = '5';
+
+delete from cursos where id = '3';
+
+delete from cursos where ano '2014';
+
+truncate cursos; 
